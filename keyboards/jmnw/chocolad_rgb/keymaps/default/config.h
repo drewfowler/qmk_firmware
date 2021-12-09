276a64d17dc7505b4a87497c6e43c8423cc48844
drewfowler17 <90705333+drewfowler17@users.noreply.github.com>
2021-12-09 15:49:11 -0700

    Add chocolad_rgb
    added with default Le Chiffre keymap

---
+++ keyboards/jmnw/chocolad_rgb/keymaps/default/config.h
@@ -0,0 +1,11 @@
+#pragma once
+//Disable unused functionality
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+// Configure the OLED screen to reduce burn-in
+//#define OLED_TIMEOUT 10000
+//#define OLED_BRIGHTNESS 80
+
+#define IGNORE_MOD_TAP_INTERRUPT //helps with homerow mods
+#define COMBO_COUNT 5
+#define COMBO_TERM 30
