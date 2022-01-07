7d8b6e42cbaa697cc1349dfd5cb16fe44dbac36e
Drew <dfowler@dynojet.com>
2022-01-07 07:12:11 -0700

    changed gui

--- keyboards/jmnw/chocolad_rgb/keymaps/default/keymap.c
+++ keyboards/jmnw/chocolad_rgb/keymaps/default/keymap.c
@@ -36,8 +36,8 @@ enum combo_events {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
     KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,                                        KC_Y,  KC_U, KC_I, KC_O, KC_P,
-    LGUI_T(KC_A),  LALT_T(KC_S),  KC_D, LSFT_T(KC_F),  KC_G,                 KC_H,  RSFT_T(KC_J),  KC_K,  RALT_T(KC_L),  RSFT_T(KC_SCLN),
-    KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,                                       KC_N,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,
+    KC_A,  KC_S,  KC_D, KC_F,  KC_G,                                         KC_H,  KC_J,  KC_K,  KC_L,  RSFT_T(KC_SCLN),
+    LGUI_T(KC_Z),  LALT_T(KC_X),  KC_C,  KC_V,  KC_B,                        KC_N,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,
           KC_LCTL, LT(1,KC_BSPC), RGB_HUI,                                  RGB_TOG,  LT(3,KC_SPC),  LT(2, KC_ENT)
   ),
   [_SYM] = LAYOUT(
