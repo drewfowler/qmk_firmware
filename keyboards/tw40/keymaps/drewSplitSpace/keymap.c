63962b571f00c188ba7ebe6fe47ac73e57ef3a8a
drewfowler17 <90705333+drewfowler17@users.noreply.github.com>
2022-02-16 08:48:00 -0700

    changed back to normal bspc

--- keyboards/tw40/keymaps/drewSplitSpace/keymap.c
+++ keyboards/tw40/keymaps/drewSplitSpace/keymap.c
@@ -33,7 +33,7 @@ enum combo_events {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT_all(
-        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_NO,
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
         LCTL_T(KC_DEL), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_UP, MO(2),
         KC_LCTRL, KC_LGUI, KC_LALT, MO(_NUM_SYM), KC_SPC, KC_NO, LT(_NAV,KC_SPACE), KC_DEL, KC_DEL, KC_LEFT, KC_DOWN, KC_RGHT
